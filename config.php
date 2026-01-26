1458f49cc3e5dcb7518831b2109e57f167fcbf68
adarwiyanto <adarwiyanto@gmail.com>
2026-01-26 08:06:52 +0700

    update dari HOPe

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+return array (
+  'app' => 
+  array (
+    'name' => 'Hope Belitung',
+    'base_url' => 'http://localhost/hope',
+  ),
+  'db' => 
+  array (
+    'host' => '127.0.0.1',
+    'port' => '3306',
+    'name' => 'hope',
+    'user' => 'root',
+    'pass' => '',
+    'charset' => 'utf8mb4',
+  ),
+  'security' => 
+  array (
+    'session_name' => 'TOKOSESS',
+  ),
+);
