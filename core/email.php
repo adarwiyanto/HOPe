e8f95fb5575d01912c0996a8d68c3c57e8141547
adarwiyanto <adarwiyanto@gmail.com>
2026-01-28 06:14:15 +0700

    Fix SMTP response handling

--- core/email.php
+++ core/email.php
@@ -21,41 +21,77 @@ function send_email_smtp(string $to, string $subject, string $body): bool {
   $remoteHost = ($secure === 'ssl') ? "ssl://{$host}" : $host;
   $remote = "{$remoteHost}:{$port}";
 
-  $fp = @stream_socket_client($remote, $errno, $errstr, 15, STREAM_CLIENT_CONNECT);
+  $context = stream_context_create([
+    'ssl' => [
+      'SNI_enabled' => true,
+      'SNI_server_name' => $host,
+    ],
+  ]);
+  $fp = @stream_socket_client($remote, $errno, $errstr, 15, STREAM_CLIENT_CONNECT, $context);
   if (!$fp) return false;
 
   stream_set_timeout($fp, 15);
 
-  $read = function() use ($fp): string {
+  $read = function() use ($fp): array {
     $data = '';
+    $code = 0;
     while (!feof($fp)) {
       $line = @fgets($fp, 515);
       if ($line === false) break;
       $data .= $line;
-      if (preg_match('/^\d{3} /', $line)) break;
+      if (preg_match('/^(\d{3}) /', $line, $match)) {
+        $code = (int)$match[1];
+        break;
+      }
     }
-    return $data;
+    return [$code, $data];
   };
 
-  $send = function(string $command) use ($fp, $read): string {
+  $send = function(string $command) use ($fp, $read): array {
     $written = @fwrite($fp, $command . "\r\n");
-    if ($written === false) return '';
+    if ($written === false) return [0, ''];
     return $read();
   };
 
-  $read();
-  $send('EHLO localhost');
+  $expect = function(array $response, array $expectedCodes): bool {
+    [$code] = $response;
+    return $code !== 0 && in_array($code, $expectedCodes, true);
+  };
+
+  if (!$expect($read(), [220])) {
+    fclose($fp);
+    return false;
+  }
+  if (!$expect($send('EHLO localhost'), [250])) {
+    fclose($fp);
+    return false;
+  }
   if ($secure === 'tls') {
-    $send('STARTTLS');
+    if (!$expect($send('STARTTLS'), [220])) {
+      fclose($fp);
+      return false;
+    }
     if (!@stream_socket_enable_crypto($fp, true, STREAM_CRYPTO_METHOD_TLS_CLIENT)) {
       fclose($fp);
       return false;
     }
-    $send('EHLO localhost');
+    if (!$expect($send('EHLO localhost'), [250])) {
+      fclose($fp);
+      return false;
+    }
   }
-  $send('AUTH LOGIN');
-  $send(base64_encode($cfg['user']));
-  $send(base64_encode($cfg['pass']));
+  if (!$expect($send('AUTH LOGIN'), [334])) {
+    fclose($fp);
+    return false;
+  }
+  if (!$expect($send(base64_encode($cfg['user'])), [334])) {
+    fclose($fp);
+    return false;
+  }
+  if (!$expect($send(base64_encode($cfg['pass'])), [235])) {
+    fclose($fp);
+    return false;
+  }
 
   $fromEmail = $cfg['from_email'];
   $fromName = $cfg['from_name'];
@@ -67,11 +103,23 @@ function send_email_smtp(string $to, string $subject, string $body): bool {
   $headers[] = 'Content-Type: text/html; charset=UTF-8';
   $headersStr = implode("\r\n", $headers);
 
-  $send("MAIL FROM:<{$fromEmail}>");
-  $send("RCPT TO:<{$to}>");
-  $send('DATA');
-  @fwrite($fp, $headersStr . "\r\n\r\n" . $body . "\r\n.\r\n");
-  $read();
+  if (!$expect($send("MAIL FROM:<{$fromEmail}>"), [250])) {
+    fclose($fp);
+    return false;
+  }
+  if (!$expect($send("RCPT TO:<{$to}>"), [250, 251])) {
+    fclose($fp);
+    return false;
+  }
+  if (!$expect($send('DATA'), [354])) {
+    fclose($fp);
+    return false;
+  }
+  $written = @fwrite($fp, $headersStr . "\r\n\r\n" . $body . "\r\n.\r\n");
+  if ($written === false || !$expect($read(), [250])) {
+    fclose($fp);
+    return false;
+  }
   $send('QUIT');
   fclose($fp);
   return true;
