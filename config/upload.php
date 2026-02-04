34cc6396a75233ac15fa0e72bad8562bc2f21675
adarwiyanto <adarwiyanto@gmail.com>
2026-02-04 18:36:11 +0700

    Load config.local.php for upload base

--- config/upload.php
+++ config/upload.php
@@ -1,6 +1,14 @@
 <?php
 if (!defined('UPLOAD_BASE')) {
+  $localConfig = __DIR__ . '/../config.local.php';
+  if (file_exists($localConfig)) {
+    require_once $localConfig;
+  }
+
   $envBase = getenv('HOPE_UPLOAD_BASE');
+  if (!$envBase && defined('HOPE_UPLOAD_BASE')) {
+    $envBase = HOPE_UPLOAD_BASE;
+  }
   $base = '';
   $root = realpath(__DIR__ . '/..');
   if ($envBase) {
