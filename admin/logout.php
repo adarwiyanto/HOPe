e5a5a29bd4b8d7ee5f71867422c2081bea5d2cb1
adarwiyanto <adarwiyanto@gmail.com>
2026-02-07 21:55:42 +0700

    Fix manager_toko logout access on admin pages

--- admin/logout.php
+++ admin/logout.php
@@ -3,6 +3,6 @@ require_once __DIR__ . '/../core/functions.php';
 require_once __DIR__ . '/../core/security.php';
 require_once __DIR__ . '/../core/auth.php';
 start_secure_session();
-require_admin();
+require_login();
 logout();
 redirect(base_url('index.php'));
